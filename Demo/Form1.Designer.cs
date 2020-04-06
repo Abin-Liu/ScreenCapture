ba4300bdebc4c86b069b0918446718a7731d67f2
Abin Liu <32606124+Abin-Liu@users.noreply.github.com>
2020-04-06 13:25:13 +0800

    0406

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -41,27 +41,27 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
 			this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-			this.pictureBox1.Location = new System.Drawing.Point(12, 29);
+			this.pictureBox1.Location = new System.Drawing.Point(12, 27);
 			this.pictureBox1.Name = "pictureBox1";
-			this.pictureBox1.Size = new System.Drawing.Size(590, 221);
+			this.pictureBox1.Size = new System.Drawing.Size(590, 204);
 			this.pictureBox1.TabIndex = 0;
 			this.pictureBox1.TabStop = false;
 			// 
 			// label1
 			// 
 			this.label1.AutoSize = true;
-			this.label1.Location = new System.Drawing.Point(71, 9);
+			this.label1.Location = new System.Drawing.Point(71, 8);
 			this.label1.Name = "label1";
-			this.label1.Size = new System.Drawing.Size(45, 13);
+			this.label1.Size = new System.Drawing.Size(41, 12);
 			this.label1.TabIndex = 1;
 			this.label1.Text = "<None>";
 			// 
 			// label2
 			// 
 			this.label2.AutoSize = true;
-			this.label2.Location = new System.Drawing.Point(12, 9);
+			this.label2.Location = new System.Drawing.Point(12, 8);
 			this.label2.Name = "label2";
-			this.label2.Size = new System.Drawing.Size(53, 13);
+			this.label2.Size = new System.Drawing.Size(59, 12);
 			this.label2.TabIndex = 3;
 			this.label2.Text = "Captured:";
 			// 
@@ -69,9 +69,9 @@
 			// 
 			this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
 			this.linkLabel1.AutoSize = true;
-			this.linkLabel1.Location = new System.Drawing.Point(463, 9);
+			this.linkLabel1.Location = new System.Drawing.Point(365, 8);
 			this.linkLabel1.Name = "linkLabel1";
-			this.linkLabel1.Size = new System.Drawing.Size(139, 13);
+			this.linkLabel1.Size = new System.Drawing.Size(173, 12);
 			this.linkLabel1.TabIndex = 4;
 			this.linkLabel1.TabStop = true;
 			this.linkLabel1.Text = "Click to capture from screen";
@@ -79,9 +79,9 @@
 			// 
 			// Form1
 			// 
-			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(614, 262);
+			this.ClientSize = new System.Drawing.Size(614, 242);
 			this.Controls.Add(this.linkLabel1);
 			this.Controls.Add(this.label2);
 			this.Controls.Add(this.label1);
