df80cb24386653f18115d7ce4930bbec94e2804e
abin-liu <abinn32@163.com>
2020-05-15 14:26:04 +0800

    CaptureAgent added

--- Demo/Form1.cs
+++ Demo/Form1.cs
@@ -6,35 +6,29 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using ScreenCapture;
 
 namespace Demo
 {
 	public partial class Form1 : Form
 	{
+		Rectangle m_rect;
 		public Form1()
 		{
 			InitializeComponent();
 		}
 
-		private void button1_Click(object sender, EventArgs e)
-		{
-			ScreenCapture.MainForm form = new ScreenCapture.MainForm();
-			form.ShowDialog(this);
-		}
-
-		private void CaptureFormScreen()
-		{
-			ScreenCapture.MainForm form = new ScreenCapture.MainForm();
-			if (form.ShowDialog(this) == DialogResult.OK)
-			{
-				pictureBox1.Image = form.CapturedBmp;
-				label1.Text = form.CapturedRect.ToString();
-			}
-		}
-
 		private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
 		{
-			CaptureFormScreen();
+			CaptureResult result = CaptureAgent.Capture(m_rect);
+			if (result == null)
+			{
+				return;
+			}
+
+			m_rect = result.Rect;
+			pictureBox1.Image = result.Image;
+			label1.Text = result.Rect.ToString();
 		}		
 
 		private void button1_Click_1(object sender, EventArgs e)
---
+++ ScreenCapture/CaptureAgent.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace ScreenCapture
+{
+	/// <summary>
+	/// 截取结果
+	/// </summary>
+	public class CaptureResult
+	{
+		/// <summary>
+		/// 截取的矩形范围
+		/// </summary>
+		public Rectangle Rect { get; set; }
+
+		/// <summary>
+		/// 截取的位图数据
+		/// </summary>
+		public Bitmap Image { get; set; }
+	}
+
+	/// <summary>
+	/// 截图静态帮助类
+	/// </summary>
+	public static class CaptureAgent
+	{
+		/// <summary>
+		/// 截取屏幕
+		/// </summary>
+		/// <param name="dpi">DPI，为0则使用设备默认值</param>
+		/// <returns>截取结果对象</returns>
+		public static CaptureResult Capture(int dpi = 0)
+		{
+			return Capture(new Rectangle(), dpi);
+		}
+
+		/// <summary>
+		/// 截取屏幕
+		/// </summary>
+		/// <param name="rect">初始矩形范围</param>
+		/// <param name="dpi">DPI，为0则使用设备默认值</param>
+		/// <returns>截取结果对象</returns>
+		public static CaptureResult Capture(Rectangle rect, int dpi = 0)
+		{
+			MainForm form = new MainForm();
+			form.CapturedRect = rect;
+			form.DPI = dpi;
+
+			if (form.ShowDialog() != DialogResult.OK)
+			{
+				return null;
+			}
+
+			return new CaptureResult()
+			{
+				Rect = form.CapturedRect,
+				Image = form.CapturedBmp,
+			};
+		}
+	}
+}
